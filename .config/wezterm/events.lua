7d0ca5f298938771d236951e200b2ea0842b1820
dsktchr <tachihara.29464@gmail.com>
2024-10-14 23:53:39 +0900

    fix:wezterm tab setting

--- .config/wezterm/events.lua
+++ .config/wezterm/events.lua
@@ -19,8 +19,6 @@ local colors = {
 wezterm.on('update-status', function(window, pane)
   local cells = {}
 
-  print(window:active_key_table())
-
   local cwd_uri = pane:get_current_working_dir()
 
   local cwd = ""
@@ -60,18 +58,6 @@ wezterm.on('update-status', function(window, pane)
 
   local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
 
-    -- Color palette for the backgrounds of each cell
-  local colors = {
-    '#3c1361',
-    '#52307c',
-    '#663a82',
-    '#7c5295',
-    '#b491c8',
-  }
-
-  -- Foreground color for the text across the fade
-  local text_fg = '#c0c0c0'
-
   -- The elements to be formatted
   local elements = {}
   -- How many cells have been formatted
@@ -80,11 +66,8 @@ wezterm.on('update-status', function(window, pane)
   -- Translate a cell into elements
   function push(text, is_last)
     local cell_no = num_cells + 1
-    table.insert(elements, { Foreground = { Color = text_fg } })
-    table.insert(elements, { Background = { Color = colors[cell_no] } })
     table.insert(elements, { Text = ' ' .. text .. ' ' })
     if not is_last then
-      table.insert(elements, { Foreground = { Color = colors[cell_no + 1] } })
       table.insert(elements, { Text = SOLID_LEFT_ARROW })
     end
     num_cells = num_cells + 1
@@ -100,54 +83,55 @@ wezterm.on('update-status', function(window, pane)
   local timer_icon = wezterm.nerdfonts.md_timer
 
   local form = {
-    { Background = { Color = colors[3] } },
     { Foreground = { Color = "" } },
-    { Text = " " .. DIRECTORY .. " " .. cwd .. " ", },
-    { Background = { Color = colors[3] } },
+    { Text = " " .. DIRECTORY .. " " .. cwd .. " " .. "|", },
     { Foreground = { Color = "" } },
-    { Text = " " .. DESKTOP .. " " .. hostname .. " ", },
-    { Background = { Color = colors[3] } },
+    { Text = " " .. DESKTOP .. " " .. hostname .. " " .. "|", },
     { Foreground = { Color = "" } },
     { Text = " " .. timer_icon .. " " .. date .. " "},
   }
 
-  -- window:set_right_status(wezterm.format(elements))
   window:set_right_status(wezterm.format(form))
 
-  local left_status = {
-    { Text = " " .. "Hello" .. " "}
-  }
-  window:set_left_status(wezterm.format(left_status))
+  local leader = ''
+  if window:leader_is_active() then
+    leader = 'LEADER'
+  end
+  window:set_left_status(wezterm.format({Text = ' ' .. leader .. ' '}))
 end)
 
--- Tab Name
-wezterm.on(
-  "format-tab-title",
-  function(tab, tabs, pane, config, hover, maxWidth)
-    local pane = tab.active_pane
-    local title = helper.basename(pane.foreground_process_name)
-    local icon = helper.get_prefer_icon(title)
+-- -- Tab Name
+-- wezterm.on(
+--   "format-tab-title",
+--   function(tab, tabs, pane, config, hover, maxWidth)
+--     local pane = tab.active_pane
+--     local title = helper.basename(pane.foreground_process_name)
+--     local icon = helper.get_prefer_icon(title)
 
-    -- colors
-    local CRUST = colors.CRUST
-    local BLUE = colors.BLUE
+--     local nerdfonts = wezterm.nerdfonts
+--     local scheme = wezterm.color_scheme
+--     -- colors
+--     local CRUST = colors.CRUST
+--     local BLUE = colors.BLUE
 
-    if tab.is_active then
-      BLUE = BLUE:lighten(0.2)
-    elseif hover then
-    end
+--     if tab.is_active then
+--       CRUST = CRUST:lighten(0.2)
+--     elseif hover then
+--     end
 
-    return {
-      { Background = { Color = CRUST } },
-      { Foreground = { Color = BLUE } },
-      { Background = { Color = BLUE } },
-      { Foreground = { Color = CRUST } },
-      { Text = ' ' .. icon .. ' ' .. title .. ' ' },
-      { Background = { Color = CRUST } },
-      { Foreground = { Color = BLUE } },
-    }
-  end
-)
+--     return {
+--       -- { Background = { Color = scheme.background } },
+--       -- { Foreground = { Color = BLUE } },
+--       { Text = nerdfonts.ple_left_half_circle_thick },
+--       -- { Background = { Color = CRUST } },
+--       -- { Foreground = { Color = BLUE } },
+--       { Text = ' ' .. title .. ' ' },
+--       -- { Background = { Color = CRUST } },
+--       -- { Foreground = { Color = BLUE } },
+--       { Text = nerdfonts.ple_right_half_circle_thick }
+--     }
+--   end
+-- )
 
 
 return {}
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -12,19 +12,20 @@ end
 
 colors.setup(config)
 
-config.leader =  { key = "a",  mods = "CTRL", timeout_milliseconds = 1000 }
+config.leader =  { key = "a",  mods = "CTRL", timeout_milliseconds = 2000, colors = {
+  compose_cursor = 'orange',
+}, }
 config.keys = keys
 config.key_tables = key_tables
 
 config.font = wezterm.font_with_fallback({
-  { family = "FiraCode Nerd Font", weight = "Regular", stretch = "Normal", style = "Normal" },
   { family = "JetBrainsMono Nerd Font", weight = "Regular", stretch = "Normal", style = "Normal" },
+  -- { family = "FiraCode Nerd Font", weight = "Regular", stretch = "Normal", style = "Normal" },
 })
 
-config.font_size = 15.5
+config.font_size = 16
 config.audible_bell = "Disabled"
 config.tab_bar_at_bottom = true
-config.tab_max_width = 32
-config.use_fancy_tab_bar = false
+config.tab_max_width = 64
 
 return config
