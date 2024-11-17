76bb3d286e9b48faeafec6c7d65d468be05fdaa9
dsktchr <tachihara.29464@gmail.com>
2024-11-17 23:00:24 +0900

    chore: 不要なplugin削除

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,35 +1,4 @@
 return {
-  -- {
-  --   "catppuccin/nvim",
-  --   name = "catppuccin",
-  --   priority = 1000,
-  --   opts = {
-  --     flavour = "macchiato",
-  --     integrations = {
-  --       cmp = true,
-  --       treesitter = true,
-  --       gitsigns = true,
-  --       lsp_saga = true,
-  --       telescope = {
-  --         enabled = true,
-  --         style = "nvchad"
-  --       }
-  --     }
-  --   },
-  --   config = function(_, opts)
-  --     require("catppuccin").setup(opts)
-  --     vim.cmd.colorscheme "catppuccin"
-  --   end
-  -- },
-  -- -- {
-  -- --   "mcchrish/zenbones.nvim",
-  -- --   dependencies = {"rktjmp/lush.nvim"},
-  -- --   priority = 1000,
-  -- --   config = function ()
-  -- --     vim.cmd.colorscheme "kanagawabones"
-  -- --   end
-  -- -- },
-  -- -- colorizer
   {
     'sainnhe/sonokai',
     lazy = false,
